c32f0ce5a2b360cbda1194485afc089edd57cb59
feifeiyu1000 <zhdyang1000@gmail.com>
2017-08-18 18:00:41 +0800

    project updateing

--- MysqlDemo/routes/users.js
+++ MysqlDemo/routes/users.js
@@ -36,6 +36,28 @@ router.get('/addUser', function(req, res, next){
 		});
 	});
 });
+// 修改用户
+router.get('/updateUser', function(req, res, next){
+	// 从连接池获取连接
+	pool.getConnection(function(err, connection){
+		// 获取前台页面传过来的参数
+		var param = req.query || req.params;
+		// 建立连接，增加一个用户信息
+		connection.query(userSQL.insert, [param.uid, param.name], function(err, result){
+			if(result){
+				result = {
+					code:200,
+					msg:'增加成功'
+				};
+			}
+			// 以json形式，把操作结果返回给前台页面
+			responseJSON(res, result);
+
+			// 释放连接
+			connection.release();
+		});
+	});
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
